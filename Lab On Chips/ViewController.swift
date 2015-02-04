908111b5df6b1a5e0397b803900d7bf82ba173ac
Xiangyue MENG <mengxiangyue@192-168-1-5.tpgi.com.au>
2015-02-04 14:19:50 +1100

    Initial Commit

---
+++ Lab On Chips/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab On Chips
+//
+//  Created by mengxiangyue on 15-2-4.
+//  Copyright (c) 2015年 UoM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
