7d3eb393be95d592bbf9e5c842ee57b4a5e4d17d
Xiangyue MENG <mengxiangyue@192-168-1-7.tpgi.com.au>
2015-02-08 23:15:32 +1100

    first commit of LoC

---
+++ Lab On Chips/PhotoThumbnailCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoThumbnailCollectionViewCell.swift
+//  Lab On Chips
+//
+//  Created by mengxiangyue on 15-2-4.
+//  Copyright (c) 2015年 UoM. All rights reserved.
+//
+
+import UIKit
+
+class PhotoThumbnailCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Lab On Chips/ViewEmail.swift
@@ -0,0 +1,35 @@
+//
+//  ViewEmail.swift
+//  Lab On Chips
+//
+//  Created by mengxiangyue on 15-2-8.
+//  Copyright (c) 2015年 UoM. All rights reserved.
+//
+
+import UIKit
+
+class ViewEmail: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
